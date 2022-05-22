359f1b801cb2c62e129c015449240510a546ca92
jaedeokhan <hjaedeok@gmail.com>
2022-05-22 23:28:49 +0900

    feat : pyscript event, plot

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+number_input = Element("number_input")
+result = Element("result")
+
+def play_game(*args):
+    user_guess = number_input.value
+    machine_guess = random.randint(1, 50)
+    if int(user_guess) == machine_guess:
+        result.element.innerText = "You win!"
+    else:
+        result.element.innerText = f"You lost! The machine chose {machine_guess}!"
+    number_input.clear()
